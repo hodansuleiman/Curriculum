9f32d22f7ccd3ff7c93048fdb384c254d609d1a3
Hodan Suleiman <suleimanhodan9@gmail.com>
2022-07-05 21:46:29 -0400

    quiz demo

---
+++ JavaScript/DOM/practice/Quiz/main.js
@@ -0,0 +1,15 @@
+const options= document.querySelector('.options')
+const feedback= document.querySelector('.feedback')
+
+
+
+options.addEventListener('click',function(e){
+    if (e.target.hasAttribute('data-correct')){
+        feedback.innerText = 'CORRECT!';
+        feedback.style.color='green'
+    } else if (e.target.tagname ==='LI'){
+        feedback.innerText = 'WRONG!'
+        feedback.style.color='crimson'
+    }
+
+});
